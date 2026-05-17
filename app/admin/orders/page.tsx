c07ceeee5093e4e64ac7ff6ca7c774eb7f906f63
MalekAloklla <mk20082030@gmail.com>
2026-05-17 14:53:34 +0400

    add admin pages

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+
+export default function OrdersPage() {
+  return (
+    <main className="min-h-screen bg-[#0b0b0b] text-white p-10">
+
+      <Link
+        href="/admin"
+        className="flex items-center gap-2 text-white/60 hover:text-[#d8cdbd] transition mb-10"
+      >
+        <ArrowLeft size={18} />
+        Back Dashboard
+      </Link>
+
+      <p className="uppercase tracking-[6px] text-[#d8cdbd] text-xs mb-4">
+        Orders
+      </p>
+
+      <h1 className="text-5xl font-black mb-10">
+        Orders Management
+      </h1>
+
+      <div className="bg-[#151515] border border-white/5 rounded-[30px] p-10 text-center text-white/40">
+        No orders yet.
+      </div>
+
+    </main>
+  );
+}
